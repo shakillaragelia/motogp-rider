f53f0a7f8f7b077d04f0ed401f9eb3cab1b1b1ba
ragel <shakilla.ragelia25@gmail.com>
2025-09-05 19:00:30 +0700

    add HomeView.vue

---
+++ src/components/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <MainLayout>
+    <h1 class="text-center">Welcome to MotoGP Rider!</h1>
+    <p class="lead text-center">Proyek Vue + Bootstrap</p>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '../components/MainLayout.vue'
+</script>
