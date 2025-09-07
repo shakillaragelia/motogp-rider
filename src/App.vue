11f60149885e0ecec7bf1261135300c9234d4506
ragel <shakilla.ragelia25@gmail.com>
2025-09-07 18:49:47 +0700

    fix double header bug at App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import MainLayout from './layouts/MainLayout.vue';
-</script>
-
+</script> 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <MainLayout></MainLayout>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <MainLayout>
+  </MainLayout>
 </template>
 
 <style scoped>
