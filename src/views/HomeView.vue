79585ea787a61ecd82546e70f411c5cde465a23a
ragel <shakilla.ragelia25@gmail.com>
2025-09-07 19:55:32 +0700

    add css to main.js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
-  <AppHeader>=nnjjj</AppHeader>
+  <AppHeader></AppHeader>
+  <RidersCards></RidersCards>
   <AppFooter></AppFooter>
 </template>
 
 <script setup>
 import AppFooter from '@/components/AppFooter.vue';
 import AppHeader from '@/components/AppHeader.vue';
+import RidersCards from '@/components/RidersCards.vue';
 </script>
