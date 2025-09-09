bb16e3e4a66b76ed842b8cc187c354865982e8ae
ragel <shakilla.ragelia25@gmail.com>
2025-09-09 22:44:48 +0700

    import view fragment to MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import AboutView from '@/views/AboutView.vue';
 import HomeView from '@/views/HomeView.vue';
+import RidersView from '@/views/RidersView.vue';
 </script>
 
 
 <template>
   <HomeView></HomeView>
   <AboutView></AboutView>
+  <RidersView></RidersView>
 </template>
