72ff8a6f0ed8b858a8631021c799225a07928cfc
ragel <shakilla.ragelia25@gmail.com>
2025-09-09 22:36:35 +0700

    add routing at TeamsView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import TeamsView from '@/views/TeamsView.vue'
+//import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,11 +14,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: AboutView,
+     // component: AboutView,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       //component: () => import('../views/AboutView.vue'),
+    },
+
+    {
+      path: '/',
+      name: 'team',
+      component: TeamsView,
     },
   ],
 })
