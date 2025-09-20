9ba1a48385a27a2d95a1e7836fc57f70fc247119
ragel <shakilla.ragelia25@gmail.com>
2025-09-20 15:26:29 +0700

    fixed routing

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div>
     <AppHeader />
     <router-view />   
+    <AppFooter />
   </div>
 </template>
 
 <script setup>
+import AppFooter from './components/AppFooter.vue';
 import AppHeader from './components/AppHeader.vue'
 </script>
 
