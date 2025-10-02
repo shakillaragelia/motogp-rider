45ca3a7e2f7f3c7dc48f57fec6d990d45b8c1202
ragel <shakilla.ragelia25@gmail.com>
2025-10-02 17:43:11 +0700

    update at app.controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rider } from './rider/rider.entity';
+import { Team } from './team/team.entity';
+import { RaceClass } from './race-class/race-class.entity';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'motogp_db',
+      entities: [Rider, Team, RaceClass],
+      synchronize: true, // ❗buat development aja, nanti produksi matiin
+    }),
+  ],
+})
+export class AppModule {}
