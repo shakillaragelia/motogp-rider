058f8daf53770d8b3206aa5cfc1cdf1c251a5049
ragel <shakilla.ragelia25@gmail.com>
2025-10-02 18:11:35 +0700

    add race-class.service

---
+++ backend/src/race-class/race-class.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RaceClass } from './race-class.entity';
+
+@Injectable()
+export class RaceService {
+  constructor(
+    @InjectRepository(RaceClass)
+    private raceClassRepository: Repository<RaceClass>,
+  ) {}
+
+  findAll(): Promise<RaceClass[]> {
+    return this.raceClassRepository.find({
+      relations: ['rider', 'team'],
+    });
+  }
+
+  create(raceClassData: Partial<RaceClass>): Promise<RaceClass> {
+    const raceClass = this.raceClassRepository.create(raceClassData);
+    return this.raceClassRepository.save(raceClass);
+  }
+}
