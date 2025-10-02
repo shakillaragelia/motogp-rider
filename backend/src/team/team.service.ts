87155b787c933603b0fc96484fad37ccdca131a0
ragel <shakilla.ragelia25@gmail.com>
2025-10-02 18:06:13 +0700

    add team.service

---
+++ backend/src/team/team.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Team } from './team.entity';
+
+@Injectable()
+export class TeamService {
+  constructor(
+    @InjectRepository(Team)
+    private teamRepository: Repository<Team>,
+  ) {}
+
+  findAll(): Promise<Team[]> {
+    return this.teamRepository.find({
+      relations: ['rider', 'raceClass'],
+    });
+  }
+
+  create(teamData: Partial<Team>): Promise<Team> {
+    const team = this.teamRepository.create(teamData);
+    return this.teamRepository.save(team);
+  }
+}
