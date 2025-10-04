7e12a5db4873359adc7919fecd3df2696edf9113
ragel <shakilla.ragelia25@gmail.com>
2025-10-04 17:56:04 +0700

    update at team.entity

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,28 +1,9 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Rider } from './rider/rider.entity';
-import { Team } from './team/team.entity';
-import { RaceClass } from './race-class/race-class.entity';
-import { RiderModule } from './rider/rider.module';
-import { TeamModule } from './team/team.module';
-import { RaceClassModule } from './race-class/race-class.module';
+import { Controller, Get } from '@nestjs/common';
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'motogp_db',
-      entities: [Rider, Team, RaceClass],
-      synchronize: true, //buat development aja, nanti produksi matiin
-    }),
-
-    RiderModule,
-    TeamModule,
-    RaceClassModule,
-  ],
-})
-export class AppModule {}
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { RiderModule } from './rider/rider.module';
+import { TeamModule } from './team/team.module';
+import { RaceClassModule } from './race-class/race-class.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '3306', 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      username: process.env.DB_USERNAME,
+      password:
+        process.env.DB_PASSWORD === '' ? undefined : process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
+    RiderModule,
+    TeamModule,
+    RaceClassModule,
   ],
 })
 export class AppModule {}
