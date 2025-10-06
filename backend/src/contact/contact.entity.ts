b29e09faa88f1c255ff4cd9912b323a78af399af
ragel <shakilla.ragelia25@gmail.com>
2025-10-06 07:00:32 +0700

    add contact.entity

---
+++ backend/src/contact/contact.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Contact {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column({ type: 'text' })
+  message: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
