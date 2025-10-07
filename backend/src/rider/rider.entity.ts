266c41a4ac01734f923a52440cf437684d15eac2
ragel <shakilla.ragelia25@gmail.com>
2025-10-08 04:08:02 +0700

    update at backend for team -> rider.entity

--- backend/src/rider/rider.entity.ts
+++ backend/src/rider/rider.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-import { Team } from '../team/team.entity';
+import { Team } from 'src/team/team.entity';
+import { RaceClass } from 'src/race-class/race-class.entity';
 
 @Entity()
 export class Rider {
@@ -9,6 +10,26 @@ export class Rider {
   @Column()
   name: string;
 
+  @Column()
+  number: number;
+
+  @Column({ nullable: true })
+  birthPlaceDate: string;
+
+  @Column({ nullable: true })
+  country: string;
+
+  @Column({ type: 'text', nullable: true })
+  bio: string;
+
+  @Column({ nullable: true })
+  image: string;
+
+  // Relasi ke Team (banyak rider dalam satu tim)
   @ManyToOne(() => Team, (team) => team.riders)
   team: Team;
+
+  // Relasi ke RaceClass (misal MotoGP, Moto2, dsb)
+  @ManyToOne(() => RaceClass, (raceClass) => raceClass.riders)
+  raceClass: RaceClass;
 }
