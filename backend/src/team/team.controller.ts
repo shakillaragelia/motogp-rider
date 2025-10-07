3a0ad76bc1230bcf264dbf67a895213aaa2ec7fe
ragel <shakilla.ragelia25@gmail.com>
2025-10-08 04:37:49 +0700

    update at backend for team -> team.controller

--- backend/src/team/team.controller.ts
+++ backend/src/team/team.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { TeamService } from './team.service';
 import { Team } from './team.entity';
 
@@ -11,8 +19,23 @@ export class TeamController {
     return this.teamService.findAll();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.teamService.findOne(id);
+  }
+
   @Post()
-  create(@Body() teamData: Partial<Team>): Promise<Team> {
-    return this.teamService.create(teamData);
+  create(@Body() data: Partial<Team>) {
+    return this.teamService.create(data);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data: Partial<Team>) {
+    return this.teamService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.teamService.remove(id);
   }
 }
