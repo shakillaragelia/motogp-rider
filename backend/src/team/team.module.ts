94f3b12a78a82f017766dd8cd2d496fe6a7a5800
ragel <shakilla.ragelia25@gmail.com>
2025-10-08 04:35:30 +0700

    update at backend for team -> team.module

--- backend/src/team/team.module.ts
+++ backend/src/team/team.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from './team.entity';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { Rider } from 'src/rider/rider.entity';
+import { RaceClass } from 'src/race-class/race-class.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team])],
-  providers: [TeamService],
+  imports: [TypeOrmModule.forFeature([Team, Rider, RaceClass])],
   controllers: [TeamController],
+  providers: [TeamService],
 })
 export class TeamModule {}
