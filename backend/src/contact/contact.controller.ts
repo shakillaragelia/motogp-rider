b7b361461176a4650d8654ab3a2cc110ce4f1bc9
ragel <shakilla.ragelia25@gmail.com>
2025-10-08 04:58:20 +0700

    update at backend for contact -> contact.controller

--- backend/src/contact/contact.controller.ts
+++ backend/src/contact/contact.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { Contact } from './contact.entity';
 
-@Controller('contact')
+@Controller('contacts')
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
@@ -11,8 +11,13 @@ export class ContactController {
     return this.contactService.findAll();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.contactService.findOne(id);
+  }
+
   @Post()
-  create(@Body() data: Partial<Contact>): Promise<Contact> {
+  create(@Body() data: Partial<Contact>) {
     return this.contactService.create(data);
   }
 
