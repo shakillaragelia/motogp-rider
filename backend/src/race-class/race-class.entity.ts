e7e476ce84760bf52acce5466ad984bfcc0bb378
ragel <shakilla.ragelia25@gmail.com>
2025-10-08 04:08:59 +0700

    update at backend for team -> race-class.entity

--- backend/src/race-class/race-class.entity.ts
+++ backend/src/race-class/race-class.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { Team } from '../team/team.entity';
+import { Team } from 'src/team/team.entity';
+import { Rider } from 'src/rider/rider.entity';
 
 @Entity()
 export class RaceClass {
@@ -7,8 +8,16 @@ export class RaceClass {
   id: number;
 
   @Column()
-  name: string;
+  name: string; // Contoh: "MotoGP", "Moto2", "Moto3", "MotoE"
 
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  // relasi ke Team (satu kelas punya banyak tim)
   @OneToMany(() => Team, (team) => team.raceClass)
   teams: Team[];
+
+  // relasi ke Rider (satu kelas bisa punya banyak rider juga)
+  @OneToMany(() => Rider, (rider) => rider.raceClass)
+  riders: Rider[];
 }
